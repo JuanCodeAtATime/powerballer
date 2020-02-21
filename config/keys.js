592af455a2d625317a6b3a3375032b96ac404a35
Juan Rivera <jrivera@primalplatoon.com>
2020-02-21 09:05:01 -0500

    MERN authentication files added to root directory.  Powerballpro subdirectory name changed to Client.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://pbProun:pbPropw@cluster0-f4smf.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://pbProun:pbPropw@cluster0-f4smf.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,24 +1,58 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 const app = express();
-const PORT = process.env.PORT || 3001;
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/powerball");
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+// const express = require("express");
+
+// const mongoose = require("mongoose");
+// // const routes = require("./routes");
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// // Define middleware here
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+// // Add routes, both API and view
+// // app.use(routes);
+
+// // Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/powerball");
+
+// // Start the API server
+// app.listen(PORT, function () {
+//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// });
