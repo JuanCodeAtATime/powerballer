8c3e12c7587b66a90407f826820c687f30979965
Juan Rivera <jrivera@primalplatoon.com>
2020-03-05 18:18:17 -0500

    Latest changes with powerball input fields on numbers page.

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,7 +6,7 @@ const keys = require("../../config/keys");
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
-// Load User model
+// Load User modelkeys
 const User = require("../../models/User");
 
 // @route POST api/users/register
