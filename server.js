372b0cb59c6d55fbb1404a8fc03f738be045ccf0
Juan Rivera <jrivera@primalplatoon.com>
2020-03-05 19:10:18 -0500

    Added code to server file to enable Heroku deployment.

--- server.js
+++ server.js
@@ -32,8 +32,14 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/numbers", numbers);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+
 
 
 // const express = require("express");
