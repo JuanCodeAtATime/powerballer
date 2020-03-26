b3571bce7b9f148c4a9cb03d77388faa1e1e0822
Juan Rivera <jrivera@primalplatoon.com>
2020-03-26 11:09:22 -0400

    Server file troubleshooting.

--- server.js
+++ server.js
@@ -46,12 +46,20 @@ mongoose
 // serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect('https://' + req.hostname + req.url);
+    } else {
+      next()
+    }
+  })
 }
+
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
