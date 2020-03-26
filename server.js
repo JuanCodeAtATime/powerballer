52b7d2c599e134ef51c607b8bf63ecf673de04ac
Juan Rivera <jrivera@primalplatoon.com>
2020-03-26 13:42:38 -0400

    Updated server file.

--- server.js
+++ server.js
@@ -38,20 +38,20 @@ mongoose
 
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-// }
-
-
-//serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  //set static folder
   app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
+
+
+// //serve static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//   //set static folder
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
