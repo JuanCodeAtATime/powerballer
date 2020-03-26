e7c915c5e04d1d78b5e6e324676f4bca612a5062
Juan Rivera <jrivera@primalplatoon.com>
2020-03-26 14:35:53 -0400

    Server file updated

--- server.js
+++ server.js
@@ -45,7 +45,6 @@ const path = require("path");
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, "client", "build")));
 
-
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
