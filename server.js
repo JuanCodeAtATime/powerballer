91fb398f8f273d0654a65959e0de58d0ddc79c41
Juan Rivera <jrivera@primalplatoon.com>
2020-03-26 14:20:12 -0400

    Installed now for zeit deployment.

--- server.js
+++ server.js
@@ -38,8 +38,17 @@ mongoose
 
 
 
+
+const path = require("path");
+//don't forget to npm install -s path
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 
