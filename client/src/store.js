40160b1b7e66ca733f84f2eca338473eae1c0ffc
Juan Rivera <jrivera@primalplatoon.com>
2020-04-01 17:48:21 -0400

    Added ternary statement in store.js file to address rendering issue on non-Chrome browsers.

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,14 @@ const initialState = {};
 
 const middleware = [thunk];
 
-
+//Line 15 and 16 (col 90-99) added to fix rendering issue on non-Chrome browsers
 const store = createStore(
     rootReducer,
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.navigator.userAgent.includes('Chrome') ?
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
     )
 );
 export default store;
